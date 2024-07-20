b9a7d018b1f7ab002dba83b3ec240c69c2961933
Zeyad Tarek <zeyad.ta01@gmail.com>
2024-07-20 04:12:30 +0300

    add needed config files

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'custom-gray': '#616161',
+      },
+    },
+  },
+  plugins: [],
+}
+
